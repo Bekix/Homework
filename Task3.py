759f07f4b00b27fb38feda50003261a585aa0fd5
Bekix <108242644+Bekix@users.noreply.github.com>
2022-06-27 16:12:31 +0300

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,15 +1,15 @@
 #Task 3
 def zeros(n):
-    s = 1
-    for i in range(1, n+1):
-        s *= i
-    s = str(s)
-    count = 0
-    i = 1
-    while s % 10 ** i == 0:
-        i += 1
-        count += 1
-    return count
+    if n != 0:
+        s = 1
+        for i in range(1, n+1):
+            s *= i
+        count = 0
+        i = 1
+        while s % 10 ** (count + 1) == 0:
+            count += 1
+        return count
+    else: return 0
 
 assert zeros(0) == 0
 assert zeros(6) == 1
