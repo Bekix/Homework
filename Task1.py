ee9756cf46ed4cb6fdc34a1196a62ee34cff59d1
Bekix <108242644+Bekix@users.noreply.github.com>
2022-06-27 16:13:01 +0300

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,13 +1,16 @@
 #Task 1
 def domain_name(url):
-    c = 0
-    while 1 == 1:
-        c += 1
-        if url[c] == '.': c += 1; break
-        if url[c] == '/': c += 2; break
-    i = c
-    while url[i] != '.': i += 1
-    return url[c:i]
+    if url[3] == '.':
+        c = 4
+    else:
+        i = 3
+        while url[i] != '.' and (url[i - 1] != '/' or url[i] != '/'):
+            i += 1
+        i += 1
+        c = i
+    for i in range(c, len(url)):
+        if url[i] == '.': v = i; break
+    return url[c:v]
 
 assert domain_name("http://google.com") == "google"
 assert domain_name("http://google.co.jp") == "google"
