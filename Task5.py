0685023b91b1fdca6fe14409fb589334eecb5094
Bekix <108242644+Bekix@users.noreply.github.com>
2022-06-28 02:43:51 +0300

    Update Task5.py

--- Task5.py
+++ Task5.py
@@ -1,48 +1,23 @@
-#Task 5
+import itertools
+import math
 def count_find_num(primesL, limit):
-    p = 1
-    count = 0
+    x = 1
+    count = 1
     for i in primesL:
-        p *= i
-    if p <= limit:
-        mx = p
-        for n in range(p, limit + 1):
-            ans = []
-            x = n
-            c = 0
-            d = 2
-            while d ** 2 <= n:
-                if n % d == 0:
-                    ans.append(d)
-                    n //= d
-                else:
-                    d += 1
-            if n > 1:
-                ans.append(n)
-            if set(ans) == set(primesL):
-                count += 1
-                mx = max(mx, x)
-        return [count, mx]
-    else:
+        x *= i
+    if x > limit:
         return []
-
-
-primesL = [2, 3]
-limit = 200
-assert count_find_num(primesL, limit) == [13, 192]
-
-primesL = [2, 5]
-limit = 200
-assert count_find_num(primesL, limit) == [8, 200]
-
-primesL = [2, 3, 5]
-limit = 500
-assert count_find_num(primesL, limit) == [12, 480]
-
-primesL = [2, 3, 5]
-limit = 1000
-assert count_find_num(primesL, limit) == [19, 960]
-
-primesL = [2, 3, 47]
-limit = 200
-assert count_find_num(primesL, limit) == []
+    mx = x
+    i = 1
+    while 1 == 1:
+        A = list(itertools.combinations_with_replacement(primesL, i))
+        c = count
+        for g in A:
+            flag = math.prod(g) * x
+            if flag <= limit:
+                count += 1
+                mx = max(mx, flag)
+        if c == count:
+            break
+        i += 1
+    return [count, mx]
